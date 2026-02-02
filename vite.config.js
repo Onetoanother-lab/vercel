aa01051ca52fb3cd827337a41cfa98747f375451
onetoanother-lab <i2369096020@gmail.com>
2026-02-02 22:03:41 +0500

    d

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
-base: '/vercel/',
-  plugins: [ tailwindcss(),react({ babel: { plugins: [['babel-plugin-react-compiler']], }, }), ],
+  base: './', // Change from '/vercel/' to './'
+  plugins: [ 
+    tailwindcss(),
+    react({ 
+      babel: { 
+        plugins: [['babel-plugin-react-compiler']], 
+      }, 
+    }), 
+  ],
   build: {
-    outDir: 'docs',
+    outDir: 'dist', // Change from 'docs' to 'dist'
     assetsDir: 'assets',
   }
 })
